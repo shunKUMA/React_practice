6aa2fc85ccd1e257814dc83934e8101b1a4578be
shunKUMA <kumachan4874@gmail.com>
2021-08-19 15:21:22 +0900

    練習中

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import ColorMessage from "./components/ColorMessage";
 
-function App() {
+const App = () => {
+  const [num, setNum] = useState(0);
+  const [faceShow, setFaceShow] = useState(false);
+
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const onClickSwitch = () => {
+    setFaceShow(!faceShow);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShow || setFaceShow(true);
+      } else {
+        faceShow && setFaceShow(false);
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [num]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 style={{ color: "red" }}>Hello World!</h1>
+      <ColorMessage color="blue">Hello my world!</ColorMessage>
+      <ColorMessage color="pink">Hello your world!</ColorMessage>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitch}>on/off</button>
+      <p>{num}</p>
+      {faceShow && <p>(*^^*)</p>}
+    </>
   );
-}
+};
 
 export default App;
